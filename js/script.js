17393834c6e286a7e4ba40e8ae00c84dc4683237
winnie <winnie.trap@gmail.com>
2020-04-24 05:43:36 +0900

    register extension

---
+++ js/script.js
@@ -0,0 +1,9 @@
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse){
+    if (request == "Action"){
+        hogehoge();
+    }
+});
+
+function hogehoge(){
+    alert("Hello World");
+}
