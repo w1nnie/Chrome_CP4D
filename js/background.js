e1cade304643ff64d6f2e37af099f4f6f887c674
winnie <winnie.trap@gmail.com>
2020-04-26 09:07:20 +0900

    display screenshot

--- js/background.js
+++ js/background.js
@@ -1,4 +1,6 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-    let imgData = chrome.tabs.captureVisibleTab(null,{format:"png"},function(string){});
-    chrome.tabs.sendMessage(tab.id, {poyo:imgData});
+    dataUrl = chrome.tabs.captureVisibleTab(null,{},function(dataUrl){
+        chrome.tabs.sendMessage(tab.id, {message: dataUrl});
+    });
+    
 });
--- js/content.js
+++ js/content.js
@@ -1,9 +1,11 @@
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
     console.log("content ni kitayo");
-    console.log(request);
-    let po = document.createElement('div');
-    po.textContent = '../image/128.png';
-    po.style = 'overflow:hidden;z-index:100;position:fixed;width:100vw;height:100vh;top:0px;left:0px;:margin:0px;padding:0px;';
+    console.log(request.message);
+    console.log(sender);
+
+    let po = document.createElement('img');
+    po.src = request.message;
+    po.style = 'overflow:hidden;z-index:100000000;position:fixed;width:100vw;height:100vh;top:0px;left:0px;:margin:0px;padding:0px;';
     document.body.appendChild(po);
     console.log("body.src ni imgData iretayo");
 }); 
