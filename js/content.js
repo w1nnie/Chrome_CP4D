128b9c29ccb144f2f65a2433985dc33c71fa0ea9
winnie <winnie.trap@gmail.com>
2020-04-27 16:12:33 +0900

    separate js and css

--- js/content.js
+++ js/content.js
@@ -2,25 +2,38 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 
     // canvasを作りbackground.jsから受け取ったimagePathを流し込み、domへ追加(全面にスクショ画像を表示)
     let canvas = document.createElement('canvas');
+    canvas.className = 'screen-shot'
     let imagePath = request.message;
     let ctx = canvas.getContext("2d");
     draw(canvas,imagePath,ctx);
-    canvas.style = 'overflow:hidden;z-index:10000;position:fixed;width:100vw;height:100vh;top:0px;left:0px;:margin:0px;padding:0px;';
     document.body.appendChild(canvas);
 
+    // マウスに追従するポップアップ
     let colorInfo = document.createElement('div');
-    colorInfo.style = 'width:200px;height:200px;z-index:10001;position:fixed;';
+    colorInfo.className = 'mouse-tracker';
     document.body.appendChild(colorInfo);
 
-    // ポインタの座標から画像のRGBA値を取得
+    // カラーサークルを表示するポップアップ
+    let colorCircles = document.createElement('div');
+    colorCircles.className = 'color-circles';
+    document.body.appendChild(colorCircles);
+
+    let hsl = document.createElement('canvas');
+    hsl.className = 'hsl';
+    colorCircles.appendChild(hsl);
+
+    // mousemoveを検知してimageDataを取得、処理
     canvas.addEventListener('mousemove',function(e){
         let mousePos = getMousePosition(e);
         console.log(mousePos);
         imageData = ctx.getImageData(mousePos.x*window.devicePixelRatio,mousePos.y*window.devicePixelRatio,1,1);
         // console.log("R:" + imageData.data[0] + " G:" + imageData.data[1] + " B:" + imageData.data[2] + " A:" + imageData.data[3]);
+
         colorInfo.style.backgroundColor = "rgba(" + imageData.data[0] + "," + imageData.data[1] + "," + imageData.data[2] + "," + imageData.data[3]/255 + ")";
         colorInfo.style.top = mousePos.y+50 + "px";
         colorInfo.style.left = mousePos.x+50 + "px";
+
+
     })
     
 
